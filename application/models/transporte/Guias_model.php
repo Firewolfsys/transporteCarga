fc3ca7adab6a32504c551ae283e4fe4e345b2d5b
jahodx <jponciano@gmail.com>
2018-09-26 17:40:14 -0600

    reporte guias

--- application/models/transporte/Guias_model.php
+++ application/models/transporte/Guias_model.php
@@ -108,4 +108,32 @@ class Guias_model extends CI_Model {
        $resultado = $consulta->result();
        return $resultado;
     }
+
+    public function obtener_guias_reporte($id_cliente, $id_estado, $fechaI, $fechaF){
+
+      $this->db->select('g.codigo_guia, g.id_cliente_envia, g.id_guia_estado, g.fecha_creacion, g.peso, c.nombre_comercial,  o.lugar lugar_origen, d.lugar lugar_destino, e.estado ');
+      $this->db->from('guias g');
+      $this->db->join('clientes c', 'g.id_cliente_envia = c.id_cliente ');
+      $this->db->join('lugares o','g.id_lugar_origen = o.id_lugar ' );
+      $this->db->join('lugares d', 'g.id_lugar_destino = d.id_lugar');
+      $this->db->join('guias_estado e', 'g.id_guia_estado = e.id_guia_estado');
+      
+  
+      if($id_cliente!=0){
+        $this->db->where('g.id_cliente_envia', $id_cliente);
+      }
+  
+      if($id_estado!=0){
+        $this->db->where('g.id_guia_estado', $id_estado);
+      }
+  
+  
+      $this->db->where('g.fecha_creacion >=', $fechaI);
+      $this->db->where('g.fecha_creacion <=', $fechaF);
+  
+      $this->db->order_by('g.fecha_creacion', 'desc');
+      $consulta = $this->db->get();
+      $resultado = $consulta->result();
+      return $resultado;
+    }
 }
