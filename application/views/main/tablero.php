ae55da3d5cf06e5ff57c8e6df05b5b98d9cbf42c
jahodx <jponciano@gmail.com>
2018-10-19 09:37:44 -0600

    graphics

--- application/views/main/tablero.php
+++ application/views/main/tablero.php
@@ -1,179 +1,286 @@
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Tablero</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Administracion</a></li>
-              <li class="breadcrumb-item active">Home</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+<!-- Content Header (Page header) -->
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0 text-dark">Tablero</h1>
+      </div><!-- /.col -->
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Administracion</a></li>
+          <li class="breadcrumb-item active">Home</li>
+        </ol>
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+  </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
 
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-          <!-- Custom tabs (Charts with tabs)-->
-            <div class="card">
-              <div class="card-header d-flex p-0">
-                <h3 class="card-title p-3">
-                  <i class="fa fa-pie-chart mr-1"></i>
-                  Seguimiento a Guias
-                </h3>
-              </div><!-- /.card-header -->
-              <div class="card-body">
-                <div class="tab-content p-0">
-        <!-- Info boxes -->
-        <div class="row">
+<!-- Main content -->
+<div class="content">
+  <div class="container-fluid">
+      <!-- Custom tabs (Charts with tabs)-->
+      <div class="card card-primary">
+        <div class="card-header d-flex p-0">
+          <h3 class="card-title p-3">
+            <i class="fa fa-pie-chart mr-1"></i>
+            Seguimiento a Guias
+          </h3>
+        </div><!-- /.card-header -->
+
+        <div class="card-body">
+          <div class="tab-content p-0">
+            <!-- Info boxes -->
+            <div class="row">
             <div class="col-12 col-sm-6 col-md-3">
-              <div class="info-box">
-                <span class="info-box-icon bg-info elevation-1"><i class="fa fa-dropbox"></i></span>
-  
-                <div class="info-box-content">
-                  <span class="info-box-text">Guias Creadas</span>
-                  <span class="info-box-number">
-                    <?php echo $datos->creadas; ?>
-                  </span>
-                </div>
-                <!-- /.info-box-content -->
-              </div>
-              <!-- /.info-box -->
+          <div class="info-box">
+            <span class="info-box-icon bg-info elevation-1"><i class="fa fa-dropbox"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Guias Creadas</span>
+              <span class="info-box-number">
+                <?php echo $datos->creadas; ?>
+              </span>
             </div>
-            <!-- /.col -->
-            <div class="col-12 col-sm-6 col-md-3">
-              <div class="info-box mb-3">
-                <span class="info-box-icon bg-primary elevation-1"><i class="fa fa-home"></i></span>
-  
-                <div class="info-box-content">
-                  <span class="info-box-text">Guias en Bodega</span>
-                  <span class="info-box-number"><?php echo $datos->bodega; ?></span>
-                </div>
-                <!-- /.info-box-content -->
-              </div>
-              <!-- /.info-box -->
-            </div>
-            <!-- /.col -->
-  
-            <!-- fix for small devices only -->
-            <div class="clearfix hidden-md-up"></div>
-  
-            <div class="col-12 col-sm-6 col-md-3">
-              <div class="info-box mb-3">
-                <span class="info-box-icon bg-success elevation-1"><i class="fa fa-truck"></i></span>
-  
-                <div class="info-box-content">
-                  <span class="info-box-text">Guias en Ruta</span>
-                  <span class="info-box-number"><?php echo $datos->transito; ?></span>
-                </div>
-                <!-- /.info-box-content -->
-              </div>
-              <!-- /.info-box -->
-            </div>
-            <!-- /.col -->
-            <div class="col-12 col-sm-6 col-md-3">
-              <div class="info-box mb-3">
-                <span class="info-box-icon bg-warning elevation-1"><i class="fa fa-thumbs-up"></i></span>
-  
-                <div class="info-box-content">
-                  <span class="info-box-text">Guias Entregadas</span>
-                  <span class="info-box-number"><?php echo $datos->entregadas; ?></span>
-                </div>
-                <!-- /.info-box-content -->
-              </div>
-              <!-- /.info-box -->
-            </div>
-            <!-- /.col -->
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
         </div>
+        <!-- /.col -->
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="info-box mb-3">
+            <span class="info-box-icon bg-primary elevation-1"><i class="fa fa-home"></i></span>
 
-       </div>
-              </div><!-- /.card-body -->
+            <div class="info-box-content">
+              <span class="info-box-text">Guias en Bodega</span>
+              <span class="info-box-number"><?php echo $datos->bodega; ?></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <!-- fix for small devices only -->
+        <div class="clearfix hidden-md-up"></div>
+
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="info-box mb-3">
+            <span class="info-box-icon bg-success elevation-1"><i class="fa fa-truck"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Guias en Ruta</span>
+              <span class="info-box-number"><?php echo $datos->transito; ?></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="info-box mb-3">
+            <span class="info-box-icon bg-warning elevation-1"><i class="fa fa-thumbs-up"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Guias Entregadas</span>
+              <span class="info-box-number"><?php echo $datos->entregadas; ?></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+    </div>
+
+    </div>
+          </div><!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    <br>
+
+<!-- Custom tabs (Charts with tabs)-->
+        <div class="card card-primary">
+          <div class="card-header d-flex p-0">
+            <h3 class="card-title p-3">
+              <i class="fa fa-pie-chart mr-1"></i>
+              Seguimiento a pendientes
+            </h3>
+          </div><!-- /.card-header -->
+          <div class="card-body">
+            <div class="tab-content p-0">
+
+      <!-- Small boxes (Stat box) -->
+    <div class="row">
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-info">
+                <div class="inner">
+                  <h3><?php echo $manifiestos->ruta; ?></h3>
+
+                  <p>Manifiestos en ruta</p>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-truck"></i>
+                </div>
+              </div>
+            </div> <!-- ./col -->
+
+
+
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-success">
+                <div class="inner">
+                  <h3><?php echo $manifiestos->finalizado; ?></h3>
+
+                  <p>Manifiestos finalizados</p>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-thumbs-up"></i>
+                </div>
+              </div>
+            </div>  <!-- ./col -->
+
+
+
+            <div class="col-lg-3 col-6"> <!-- small box -->
+              <div class="small-box bg-warning">
+                <div class="inner">
+                  <h3><?php echo $datos->transito; ?></h3>
+
+                  <p>Guias pendientes de entrega</p>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-calendar-plus-o"></i>
+                </div>
+              </div>
+            </div> <!-- ./col -->
+
+
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-danger">
+                <div class="inner">
+                  <h3><?php echo $manifiestos->pendientes_cierre; ?></h3>
+                  <p>Manifiestos pendientes de cierre</p>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-map"></i>
+                </div>
+              </div>
+            </div><!-- ./col -->
+          </div><!-- /.row -->
+        </div>
+      </div><!-- /.card-body -->
+    </div><!-- /.card -->
+    <br>
+
+
+
+        <div class="row">
+          <div class="col-md-6">
+            <!-- AREA CHART -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Guias Semanal</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <div class="chart">
+                  <canvas id="areaChart" style="height:250px"></canvas>
+                </div>
+              </div>
+              <!-- /.card-body -->
             </div>
             <!-- /.card -->
-        <br>
 
-  <!-- Custom tabs (Charts with tabs)-->
-            <div class="card">
-              <div class="card-header d-flex p-0">
-                <h3 class="card-title p-3">
-                  <i class="fa fa-pie-chart mr-1"></i>
-                  Seguimiento a pendientes
-                </h3>
-              </div><!-- /.card-header -->
+            <!-- DONUT CHART -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Guias Mensual</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+              </div>
               <div class="card-body">
-                <div class="tab-content p-0">
-
-         <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h3><?php echo $manifiestos->ruta; ?></h3>
-
-                <p>Manifiestos en ruta</p>
+                <canvas id="pieChart" style="height:250px"></canvas>
               </div>
-              <div class="icon">
-                <i class="fa fa-truck"></i>
-              </div>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h3><?php echo $manifiestos->finalizado; ?></h3>
-
-                <p>Manifiestos finalizados</p>
-              </div>
-              <div class="icon">
-                <i class="fa fa-thumbs-up"></i>
-              </div>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h3><?php echo $datos->transito; ?></h3>
-
-                <p>Guias pendientes de entrega</p>
-              </div>
-              <div class="icon">
-                <i class="fa fa-calendar-plus-o"></i>
-              </div>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h3><?php echo $manifiestos->pendientes_cierre; ?></h3>
-
-                <p>Manifiestos pendientes de cierre</p>
-              </div>
-              <div class="icon">
-                <i class="fa fa-map"></i>
-              </div>
-            </div>
-          </div>
-          <!-- ./col -->
-        </div>
-        <!-- /.row -->
-               </div>
-              </div><!-- /.card-body -->
+              <!-- /.card-body -->
             </div>
             <!-- /.card -->
-     
-      </div>
-      <!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
+
+
+            <!-- DONUT CHART -->
+              <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Manifiestos Mensual</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <canvas id="pieChart" style="height:250px"></canvas>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+          </div>
+          <!-- /.col (LEFT) -->
+          <div class="col-md-6">
+            <!-- LINE CHART -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Facturas Semanal</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <div class="chart">
+                  <canvas id="lineChart" style="height:250px"></canvas>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+            <!-- BAR CHART -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Facturas Mensual</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <div class="chart">
+                  <canvas id="barChart" style="height:250px"></canvas>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+          </div>
+          <!-- /.col (RIGHT) -->
+        </div>
+
+  </div><!-- /.container-fluid -->
+</div><!-- /.content -->
