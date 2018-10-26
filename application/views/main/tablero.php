7ed83ca519fa156472b95add8e8d3f2d7f82c3f8
jahodx <jponciano@gmail.com>
2018-10-26 17:27:16 -0600

    grafica 26102018

--- application/views/main/tablero.php
+++ application/views/main/tablero.php
@@ -2,6 +2,53 @@
 <?php $this->load->view('main/Encabezado')?> 
 <!-- /.Encabezado -->
 
+
+<style>
+  
+.chart_ul_x {
+   list-style: none;
+
+}
+.chart_ul_li_x {
+  text-align:center;
+  display: block;
+  padding-left: 30px;
+   position: relative;
+   margin-bottom: 4px;
+   border-radius: 5px;
+   padding: 2px 8px 2px 28px;
+   font-size: 14px;
+   cursor: default;
+   height:20px;
+   width: 140px;
+   -webkit-transition: background-color 200ms ease-in-out;
+   -moz-transition: background-color 200ms ease-in-out;
+   -o-transition: background-color 200ms ease-in-out;
+   transition: background-color 200ms ease-in-out; */
+}
+
+.chart_li_span_x
+ {
+   display: block;
+   position: absolute;
+   left: 0;
+   top: 0;
+   width: 20px;
+   border-radius: 5px;
+   height:20px
+ }
+
+ .chart_li_spanbar_x span.bar-legend-text
+ {
+   left:25px;
+   width:120px;
+ }
+ /* #chart-area > *{
+   float:left
+     } */
+
+</style>
+
 <!-- Content Header (Page header) -->
 <div class="content-header">
   <div class="container-fluid">
@@ -181,7 +228,7 @@
 
 
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-md-6">
             <!-- AREA CHART -->
             <div class="card card-primary">
               <div class="card-header">
@@ -195,7 +242,8 @@
               </div>
               <div class="card-body">
                 <div class="chart">
-                  <canvas id="guiaSemanal" style="height:250px"></canvas>
+                  <div id="legend"></div>
+                  <canvas id="guiaSemanal" style="height: 250px;"></canvas>
                 </div>
               </div>
               <!-- /.card-body -->
@@ -220,9 +268,29 @@
             </div>
             <!-- /.card -->
 
+            <!-- AREA CHART -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Guias Semestral</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <div class="chart">
+                  <canvas id="areaChart" style="height:250px"></canvas>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
           </div>
           <!-- /.col (Center) -->
-          <div class="col-md-4">
+          <div class="col-md-6">
             <!-- LINE CHART -->
             <div class="card card-primary">
               <div class="card-header">
@@ -269,25 +337,7 @@
           <!-- /.col (Rigth) -->
           <div class="col-md-4">
 
-            <!-- AREA CHART -->
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Guias Semestral</h3>
 
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                </div>
-              </div>
-              <div class="card-body">
-                <div class="chart">
-                  <canvas id="areaChart" style="height:250px"></canvas>
-                </div>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
 
 
             <!-- DONUT CHART -->
@@ -341,30 +391,101 @@
 
     function setBarData(obj)
     {
+      var colorData        = [
+      {
+        color    : '#f56954',
+        highlight: '#f56954',
+      },
+      {
+        color    : '#00a65a',
+        highlight: '#00a65a',
+      },
+      {
+        color    : '#f39c12',
+        highlight: '#f39c12',
+      },
+      {
+        color    : '#00c0ef',
+        highlight: '#00c0ef',
+      },
+      {
+        color    : '#3c8dbc',
+        highlight: '#3c8dbc',
+      },
+      {
+        color    : '#d2d6de',
+        highlight: '#d2d6de',
+      }
+    ]
+
+      var color = Math.floor(Math.random() * 6);
+
       var datos    =     {
           label               : obj.guia_descripcion,
-          fillColor           : '#053828a',
+          fillColor           : colorData[color].color,
+          strokeColor         : colorData[color].color,
+          pointColor          : colorData[color].color,
+          pointStrokeColor    : colorData[color].color,
+          pointHighlightFill  : colorData[color].highlight,
+          pointHighlightStroke: colorData[color].highlight,
           data                : obj.data
         }
+        //alert(datos.label);
         return datos;
+    }
+
+    function setChartOptions()
+    {
+      var barChartOptions  = 
+      {
+        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+        scaleBeginAtZero        : true,
+        //Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines      : true,
+        //String - Colour of the grid lines
+        scaleGridLineColor      : 'rgba(0,0,0,.05)',
+        //Number - Width of the grid lines
+        scaleGridLineWidth      : 1,
+        //Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
+        //Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines  : true,
+        //Boolean - If there is a stroke on each bar
+        barShowStroke           : true,
+        //Number - Pixel width of the bar stroke
+        barStrokeWidth          : 2,
+        //Number - Spacing between each of the X value sets
+        barValueSpacing         : 5,
+        //Number - Spacing between data sets within X values
+        barDatasetSpacing       : 1,
+        //String - A legend template
+        legendTemplate          : '<ul class="list-inline"><% for (var i=0; i<datasets.length; i++){%><li class="list-inline-item"><span class="badge " style="background-color:<%=datasets[i].fillColor%>"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>',
+        //Boolean - whether to make the chart responsive
+        responsive              : true,
+        maintainAspectRatio     : true,
+        animation: false
+      }
+      barChartOptions.datasetFill = false;
+      return barChartOptions;
+
     }
 
     function setGuiaSemanal(obj){
       var Labels = [];
       var datos = [];
       
-
+      //Obtener los nombres de las columnas
       $.each(obj.guia_semanal.dias, function(key, data) {
         Labels.push(data.diaNombre);
       });
 
-
+      //Creo la variable de los datos
       var areaChartData = {
       labels  : Labels,
-      datasets: [
-                ]
+      datasets: [ ]
       }
 
+      //Asigno cada una de las barras al dataset.
       $.each(obj.guia_semanal.estados, function (key, data){
         areaChartData.datasets.push(setBarData(data));
       });
@@ -375,52 +496,20 @@
     //- BAR CHART -
     //-------------
     var barChartCanvas                   = $('#guiaSemanal').get(0).getContext('2d')
-    var barChart                         = new Chart(barChartCanvas)
     var barChartData                     = areaChartData
-    var barChartOptions                  = {
-      responsive : true,
-      legend : {
-        position : 'top',
-      },
-      title : {
-        display : true,
-        text: 'Guia Semanal'
-      }
-      // //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-      // scaleBeginAtZero        : false,
-      // //Boolean - Whether grid lines are shown across the chart
-      // scaleShowGridLines      : true,
-      // //String - Colour of the grid lines
-      // scaleGridLineColor      : 'rgba(0,0,0,.05)',
-      // //Number - Width of the grid lines
-      // scaleGridLineWidth      : 1,
-      // //Boolean - Whether to show horizontal lines (except X axis)
-      // scaleShowHorizontalLines: false,
-      // //Boolean - Whether to show vertical lines (except Y axis)
-      // scaleShowVerticalLines  : false,
-      // //Boolean - If there is a stroke on each bar
-      // barShowStroke           : true,
-      // //Number - Pixel width of the bar stroke
-      // barStrokeWidth          : 2,
-      // //Number - Spacing between each of the X value sets
-      // barValueSpacing         : 5,
-      // //Number - Spacing between data sets within X values
-      // barDatasetSpacing       : 1,
-      // //String - A legend template
-      // legendTemplate          : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
-      // //Boolean - whether to make the chart responsive
-      // responsive              : true,
-      // maintainAspectRatio     : true
-    }
+    var barChartOptions                  = setChartOptions();
 
-    //barChartOptions.datasetFill = false
-    barChart.Bar(barChartData, barChartOptions)
-
+    var barChart  = new Chart(barChartCanvas).Bar(barChartData, barChartOptions);
+    var legendHolder = document.createElement('div');
+    legendHolder.innerHTML = barChart.generateLegend();
+    alert(legendHolder.innerHTML );
+    document.getElementById('legend').appendChild(legendHolder.firstChild);
     }
 
 
   
   })
+  
 </script>
 
 
